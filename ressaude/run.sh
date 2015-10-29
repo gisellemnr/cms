f8301b220ef82ef035d129f90b8db2ad66c83e07
Giselle Reis <giselle.mnr@gmail.com>
2015-10-29 22:35:08 +0100

    Changing scrapy's export format

--- ressaude/run.sh
+++ ressaude/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Crawls the websites for the municipal health councils of BH and SP collecting
-# all resolutions and stores them in the respective xml files.
-# OBS: The xml format is being used because the json exporter does not seem to
-# support utf-8 encoding.
-scrapy crawl RessaudeBH -o bh.xml
-scrapy crawl RessaudeSP -o sp.xml
+# all resolutions and stores them in the respective csv files.
+
+# OBS: Using CSV because it can be imported into the tables by phpMyAdmin
+
+scrapy crawl RessaudeBH -o bh.csv
+scrapy crawl RessaudeSP -o sp.csv
 
 
