3322ae6226f639195f07ef30695de9f73bc224a6
Giselle Reis <giselle.mnr@gmail.com>
2015-11-03 00:21:47 +0100

    Changing encoding to utf-8

--- ressaude/ressaude/items.py
+++ ressaude/ressaude/items.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 # Define here the models for your scraped items
 #
--- ressaude/ressaude/settings.py
+++ ressaude/ressaude/settings.py
@@ -1,4 +1,4 @@
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 # Scrapy settings for ressaude project
 #
